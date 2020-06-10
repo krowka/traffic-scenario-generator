e5cd6c210f420106ef977613976a494f4a93546c
krowka1 <lukaszkrowka98@gmail.com>
2020-06-10 14:22:10 +0200

    add generic method to keep code DRY ;)

--- src/main/java/generator/RandomSubclassGenerator.java
+++ src/main/java/generator/RandomSubclassGenerator.java
@@ -1,9 +1,14 @@
 package generator;
 
+import org.protege.owl.codegeneration.impl.WrappedIndividualImpl;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
 import project.MyFactory;
 import project.Vehicle;
+import project.Weather;
+import project.impl.DefaultVehicle;
+import project.impl.DefaultWeather;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -14,33 +19,74 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class RandomSubclassGenerator {
+    private final String iriPrefix = "http://webprotege.stanford.edu/";
     private MyFactory factory;
-    private final OWLReasoner reasoner;
-    private final OWLDataFactory dataFactory;
+    private OWLOntology ontology;
+    private OWLOntologyManager manager;
+    private OWLReasoner reasoner;
+    private OWLDataFactory dataFactory;
+    private Random random;
 
-    public RandomSubclassGenerator(OWLReasoner reasoner, MyFactory factory, OWLDataFactory dataFactory) {
-        this.reasoner = reasoner;
+    public RandomSubclassGenerator(OWLOntology ontology, MyFactory factory, OWLReasoner reasoner) {
+        this.ontology = ontology;
         this.factory = factory;
-        this.dataFactory = dataFactory;
+        this.reasoner = reasoner;
+        manager = ontology.getOWLOntologyManager();
+        dataFactory = manager.getOWLDataFactory();
+        random = new Random();
     }
 
+    public RandomSubclassGenerator(OWLOntology ontology, MyFactory factory) {
+        this(ontology, factory, new StructuralReasonerFactory().createReasoner(ontology));
+    }
+
+    // default method for basic IRI
     public Vehicle generateVehicleSubclass() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Function<String, String> toCapitalLetter = s -> s.substring(0, 1).toUpperCase() + s.substring(1);
-        OWLClass type = dataFactory.getOWLClass(IRI.create("http://webprotege.stanford.edu/vehicle"));
-        List<OWLClass> subClassList = new ArrayList<>(reasoner.getSubClasses(type, true).getFlattened());
-        System.out.println(subClassList);
-        List<String> classNames = subClassList.stream()
-                .map(OWLObject::toString)
-                .map(s -> s.substring(s.lastIndexOf("/") + 1, s.length() - 1))
-                .map(toCapitalLetter)
-                .collect(Collectors.toList());
+        return generateVehicleSubclass("vehicle");
+    }
+
+    // method for customized IRI
+    public Vehicle generateVehicleSubclass(String iriName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return generateSubclass(DefaultVehicle.class, iriName);
+    }
+
+    public Weather generateWeatherSubclass() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return generateWeatherSubclass("weather");
+    }
+
+    // method for customized IRI
+    public Weather generateWeatherSubclass(String iriName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return generateSubclass(DefaultWeather.class, iriName);
+    }
+
+    private <T extends WrappedIndividualImpl> T generateSubclass(Class<T> clazz, String iriName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        String iriString = iriPrefix + iriName;
+//        OWLClass type = dataFactory.getOWLClass(IRI.create("http://webprotege.stanford.edu/vehicle"));
+        OWLClass type = dataFactory.getOWLClass(IRI.create(iriString));
+        List<String> classNames = getSubclassesNames(type);
         System.out.println(classNames);
-        String randomSubclassName = classNames.get(new Random().nextInt(subClassList.size()));
+        String randomSubclassName = classNames.get(new Random().nextInt(classNames.size()));
         Method method = factory.getClass().getDeclaredMethod("create" + randomSubclassName, String.class);
         System.out.println(method);
-        Vehicle vehicle = (Vehicle) method.invoke(factory, "reflect_Vehicle");
-        System.out.println(vehicle.getOwlIndividual());
-        System.out.println(vehicle);
-        return vehicle;
+        // TODO change created instances name
+        @SuppressWarnings("unchecked")
+        T subclassInstance = (T) method.invoke(factory, "reflect_" + randomSubclassName);
+        System.out.println(subclassInstance.getOwlIndividual());
+        System.out.println(subclassInstance);
+        return subclassInstance;
+    }
+
+    private List<String> getSubclassesNames(OWLClass baseClass) {
+        Function<String, String> toCapitalLetter = s -> s.substring(0, 1).toUpperCase() + s.substring(1);
+        Function<String, String> extractSimpleName = fullName -> fullName
+                .substring(fullName.lastIndexOf("/") + 1, fullName.length() - 1);
+
+        List<OWLClass> subClassList = new ArrayList<>(reasoner.getSubClasses(baseClass, true).getFlattened());
+        System.out.println(subClassList);
+        return subClassList.stream()
+                .map(OWLClass::toString)
+                .map(extractSimpleName)
+                .map(toCapitalLetter)
+                .collect(Collectors.toList());
     }
 }
